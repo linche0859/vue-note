48090d91ece4cbb2cc7e1042a8194516c132245f
che.lin <aa10327tw@gmail.com>
2021-03-21 15:29:34 +0800

    fix: nuxt 生命週期的選單路徑問題

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -137,7 +137,7 @@ module.exports = {
       },
       {
         title: 'NUXT',
-        children: ['/nuxt/pm2', '/nuxt/serverMiddleware', 'lifecycle'],
+        children: ['/nuxt/pm2', '/nuxt/serverMiddleware', '/nuxt/lifecycle'],
       },
     ],
   },
